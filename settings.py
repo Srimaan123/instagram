3f89de92dde763b6de28ca9e5e2a28024106d54d
Srimaan123 <lagishettiharini124@gmail.com>
2026-08-02 12:54:20 +0000

    1/8/26

--- settings.py
+++ settings.py
@@ -6,5 +6,16 @@ settings_bp = Blueprint("settings",__name__)
 
 @settings_bp.route("/settings/<username>")
 def settings(username):
-    return render_template("settings.html",username=username)
+    with sqlite3.connect("data.db") as conn:
+        cursor = conn.cursor()
+        is_private = cursor.execute("select is_private from users where username=?",(username,)).fetchone()
+    accountType = []
+    if is_private == 'True':
+        accountType.append('Private')
+        accountType.append('Public')
+    else:
+        accountType.append('Public')
+        accountType.append('Private')
+        
+    return render_template("settings.html",username=username,accountType=accountType)
 
